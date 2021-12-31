8190aaebb6d0bbcabfdf34c9ed43a0338b780c9a
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-30 19:32:44 -0500

    adquiriendo personas de la base de datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import {getPersons} from './services/persons'
 function App() {
-  const [count, setCount] = useState(0)
-  getPersons()
+  const [count, setCount] = useState([])
+  useEffect(()=>{
+
+    getPersons()
+      .then(res => setCount(res))
+  },[])
+  
+    
+  // console.log(a)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        todo
+        {
+          count.map(oe => <p key={oe.id}>{oe.name}</p>)
+        }
       </header>
     </div>
   )
