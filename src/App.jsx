9e550ea68b97b4b86ee233a6d63b477c98f48ecc
Diego Fernando Alayon <dfar.41@gmail.com>
2022-01-20 20:28:04 -0500

    creando funcionalidad para actualizar los datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import {getPersons} from './services/persons'
+import {getPersons, updateScore} from './services/persons'
 import Users from './components/Users'
 function App() {
   const [count, setCount] = useState([])
+  const [update, setUpdate] = useState(0)
   useEffect(()=>{
-
+    
     getPersons()
       .then(res => setCount(res))
-  },[])
+      
+  },[update])
   
-  const updateScore = (id) => {
+  const handleUpdateScore = (id) => {
     //pedir la info de cada user y luego actualizar mandando esta info mas la nueva, para evitar que se sobre escriba,
+    const person = count.find(p => p.id === 3)
+    const personUpdate = {
+      ...person,
+      score : [...person.score,999]
+    }
+    console.log(personUpdate)
+    // getPerson(2)
+    //   .then(res => console.log(res))
+    updateScore(3,personUpdate)
+    setUpdate(actual => actual + 1)
+    console.log(update)
   }
   // console.log(a)
   return (
     <div className="App">
       <header >
-      
+      <button onClick={handleUpdateScore}>aoeu</button>
       </header>
       <div className="App-header">
         {
--- src/services/persons.jsx
+++ src/services/persons.jsx
@@ -6,14 +6,10 @@ export const getPersons = async () => {
  return data  
 }
 
-// export const updateScore = async (id, score) => {
-//   const person = await axios.get(`${baseUrl}/${id}`)
-//   const personUpdate = {
-//     ...person,
-//     ...person.score = [...person.]
-//   }
-//   await axios.put(`${baseUrl}/${id}`,score )
-// }
+export const updateScore = async (id, data) => {
+  console.log(id,data)
+  await axios.put(`${baseUrl}/${id}`, data )
+}
 
 export const getPerson = async (id) => {
   const {data} = await axios.get(`${baseUrl}/${id}`)
