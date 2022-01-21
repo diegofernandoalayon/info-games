9840fbb222bb427eb2abb70e12b3b22c684da61e
Diego Fernando Alayon <dfar.41@gmail.com>
2022-01-21 13:47:34 -0500

    agregando funcionalidad al FormScore

--- src/components/FormScore/index.jsx
+++ src/components/FormScore/index.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react"
 
-const FormScore = () => {
+const FormScore = ({id, update}) => {
+  const [score, setScore] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
+    if(score){
+      const newScore = score.replace(',','.')
+      if(!isNaN(newScore)){
+        update(id, Number(newScore))
+      }
+    }
+    setScore('')
+  }
+
+  const handleChange = (event) => {
+    setScore(event.target.value)
   }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input />
-        <button onClick={()=> console.log('hola')}>Save</button>
+        <input value={score} onChange={handleChange}/>
+        <button onClick={handleSubmit}>Save</button>
+        {score}
       </form>
     </>
   )
