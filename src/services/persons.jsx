197afc99899c3e810b7db8db1d577b06474b120c
Diego Fernando Alayon <dfar.41@gmail.com>
2022-01-23 21:36:12 -0500

    cambiando la forma de exportar los servicios

--- src/services/persons.jsx
+++ src/services/persons.jsx
@@ -1,23 +1,24 @@
 import axios from "axios"
 const baseUrl = 'http://localhost:3002/persons'
 
-export const getPersons = async () => {
+const getPersons = async () => {
  const {data} = await axios.get(baseUrl)
  return data  
 }
 
-export const updateScore = async (id, data) => {
+const updateScore = async (id, data) => {
   
   const status = await axios.put(`${baseUrl}/${id}`, data )
   return status
 }
 
-export const getPerson = async (id) => {
+const getPerson = async (id) => {
   const {data} = await axios.get(`${baseUrl}/${id}`)
   return data
 }
-
-export const createPerson = async (newPerson) => {
+const createPerson = async (newPerson) => {
   const {data} = await axios.post(baseUrl,newPerson)
   console.log(data)
 }
+
+export default { getPersons, updateScore, getPerson, createPerson}
