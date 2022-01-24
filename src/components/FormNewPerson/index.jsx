2e4ebf98f770d97fa897b5d8c0916c732aa975b9
Diego Fernando Alayon <dfar.41@gmail.com>
2022-01-23 21:55:10 -0500

    creando componente FormNewPerson

---
+++ src/components/FormNewPerson/index.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import personServices from '../../services/persons'
+
+const FormNewPerson = ({setUsers}) => {
+  const [name, setName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: name,
+      score: [],
+    }
+    personServices
+      .createPerson(newPerson)
+      .then((data)=>{
+        console.log('data', data)
+        setUsers((actual) => {
+          return [...actual, data]})
+      })
+    
+    setName('')
+
+    
+  }
+  return(
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={(event) => setName(event.target.value) }/>
+        <button>Create</button>
+      </form>
+    </>
+  )
+}
+export default FormNewPerson
