62aebb9b023871920de6cf4a908e498c3793e42e
Diego Fernando Alayon <dfar.41@gmail.com>
2022-02-03 00:16:44 -0500

    parametrizando los styled-components

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components'
 
-const Button = styled.button`
-  background-color: ${props => props.bgColor || '#d195d3'};
+const Button = styled.button.attrs(props => ({
+  size: props.size || '0.5em',
+  bgColor: props.bgColor || '#d195d3'
+}))`
+  background-color: ${props => props.bgColor};
   color: #fff;
   border: none;
   border-radius: 8px;
+  margin:  ${props => props.size};
+  padding: ${props => props.size};
   cursor: pointer;
   &:hover{
     background-color: #d194d2;
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -3,9 +3,11 @@ import styled from 'styled-components'
 const Input = styled.input.attrs(props => ({
   type: 'text',
 
-  size: props.size || '1em'
+  size: props.size || '0.5em'
 }))`
   color: #d194d2;
   border: 1px solid #d194d2;
+  margin: ${props => props.size};
+  padding: ${props => props.size};
 `
 export default Input
